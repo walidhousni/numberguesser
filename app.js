e14b4f1c36e45ba8670677fc1b832636e245a938
Walid Housni <w.housni22@gmail.com>
2018-08-24 01:06:48 +0100

    littlefix

--- app.js
+++ app.js
@@ -1,11 +1,4 @@
-/*
-GAME FUNCTION:
-- Player must guess a number between a min and max
-- Player gets a certain amount of guesses
-- Notify player of guesses remaining
-- Notify the player of the correct answer if loose
-- Let player choose to play again
-*/
+
 
 // Game values
 let min = 1,
